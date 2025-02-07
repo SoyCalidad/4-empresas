1dc4bf70dc2ba73b7f9eee7711c0f1d28090b43f
Soy Calidad <soycalidad.20@gmail.com>
2025-02-07 09:30:29 -0500

    [ADD] 4_empresas_context

---
+++ 4_empresas_context/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': '4 empresas Context',
+    'version': '1.0',
+    'description': '4 empresas Context',
+    'summary': '4 empresas Context',
+    'author': 'Soy Calidad',
+    'website': 'https://www.soycalidad.com',
+    'license': 'Other proprietary',
+    'category': 'crm',
+    'depends': ['base', 'mgmtsystem_context'],
+    'data': [
+        'views/context_system_views.xml',
+        'views/menus.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
