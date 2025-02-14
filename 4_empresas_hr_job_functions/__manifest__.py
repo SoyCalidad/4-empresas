f9beb3004cedbc87414211de4dd63549e458f5a2
Soy Calidad <soycalidad.20@gmail.com>
2025-02-14 12:40:53 -0500

    [ADD] 4_empresas_hr_job_functions

---
+++ 4_empresas_hr_job_functions/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': '4 empresas HR Job Functions',
+    'version': '1.0',
+    'description': '4 empresas HR Job Functions',
+    'summary': '4 empresas HR Job Functions',
+    'author': 'Soy Calidad',
+    'website': 'https://www.soycalidad.com',
+    'license': 'Other proprietary',
+    'category': 'Uncategorized',
+    'depends': ['base', 'hr_job_functions'],
+    'data': [
+        'views/hr_job_function_views.xml',
+        'views/menus.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
