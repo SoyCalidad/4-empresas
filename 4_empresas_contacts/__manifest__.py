15d71e090bea83075cd78103ff795b73ecd90541
Soy Calidad <soycalidad.20@gmail.com>
2025-03-13 13:44:33 -0500

    [FIX] 4_empresas: general improvements in partner qualification & contacts

---
+++ 4_empresas_contacts/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': '4 Empresas Contacts',
+    'version': '1.0',
+    'description': '4 Empresas Contacts',
+    'summary': '4 empresas Contacts',
+    'author': 'Soy Calidad',
+    'website': 'https://www.soycalidad.com',
+    'license': 'Other proprietary',
+    'category': 'Uncategorized',
+    'depends': ['base'],
+    'data': [
+        'views/res_partner_views.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ 4_empresas_partner_qualification/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': '4 Empresas Calificación para socios',
+    'version': '1.0',
+    'description': '4 Empresas Calificación para socios',
+    'summary': '4 Empresas Calificación para socios',
+    'author': 'Soy Calidad',
+    'website': 'https://www.soycalidad.com',
+    'license': 'Other proprietary',
+    'category': 'Uncategorized',
+    'depends': ['base', 'mgmtsystem_partner_qualification'],
+    'data': [
+
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
