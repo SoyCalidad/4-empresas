b87e52267ff7e08f0b615531f692667614612f8c
Soy Calidad <soycalidad.20@gmail.com>
2025-03-27 12:22:32 -0500

    [IMP] 4_empresas_hr_job_functions: add degree value

---
+++ 4_empresas_hr_job_functions/models/hr_job.py
@@ -0,0 +1,17 @@
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError, ValidationError
+
+
+
+class JobProfile(models.Model):
+    _inherit = 'hr.job.profile'
+
+    degree = fields.Selection([
+        ('none', 'Sin estudios'),
+        ('elementary', 'Primaria completa'),
+        ('high_school', 'Secundaria completa'),
+        ('professional_training', 'Formación profesional'),
+        ('college', 'Superior'),
+        ('master', 'Maestría'),
+        ('doctor', 'Doctorado')
+    ])
