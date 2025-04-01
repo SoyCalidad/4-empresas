3b2193057dfd1e5a45b2f1f3d2d7f9b7c05b93c8
Soy Calidad <soycalidad.20@gmail.com>
2025-04-01 12:36:13 -0500

    [FIX] 4_empresas_mail_multicompany: validate email format and encode company name in email_from

--- 4_empresas_mail_multicompany/models/mail_message.py
+++ 4_empresas_mail_multicompany/models/mail_message.py
@@ -1,5 +1,7 @@
 from odoo import api, fields, models
+from email.header import Header
 from email.utils import formataddr
+import re
 
 
 class MailMessage(models.Model):
@@ -30,7 +32,15 @@ class MailMessage(models.Model):
                         vals["reply_to"] = vals["email_from"]
 
                     company_name = self.sudo().env["res.company"].browse(vals["company_id"]).name
-                    formatted_email_from = formataddr((company_name, mail_server.smtp_user))
-                    vals["email_from"] = formatted_email_from
+
+                    def is_valid_email(email):
+                        return bool(re.match(r"[^@]+@[^@]+\.[^@]+", email))
+
+                    if is_valid_email(mail_server.smtp_user):
+                        company_name_encoded = str(Header(company_name, 'utf-8'))
+                        formatted_email_from = formataddr((company_name_encoded, mail_server.smtp_user))
+                        vals["email_from"] = formatted_email_from
+                    else:
+                        vals["email_from"] = "no-reply@example.com"
 
         return super(MailMessage, self).create(values_list)
