302ab27e8b845e7ca3b62e8b0809c973709c2cb1
Dave <fernando.gam@outlook.com>
2025-10-06 17:08:31 -0500

    override unlink method

--- 4_empresas_maintenance/models/maintenance.py
+++ 4_empresas_maintenance/models/maintenance.py
@@ -5,3 +5,10 @@ class MaintenanceEquipment(models.Model):
     _inherit = 'maintenance.equipment'
 
     type_id = fields.Many2one('maintenance.equipment.type', string='Tipo')
+    
+    def unlink(self):
+        if 'active' in self._fields:
+            if self.active == True:
+                self.write({'active': False})
+                return True
+        return super().unlink()
--- 4_empresas_maintenance/models/maintenance_type.py
+++ 4_empresas_maintenance/models/maintenance_type.py
@@ -8,3 +8,10 @@ class MaintenanceEquipmentType(models.Model):
     name = fields.Char(string='Nombre')
     sequence = fields.Integer(string='Secuencia', default=10)
     active = fields.Boolean(string='Activo', default=True)
+    
+    def unlink(self):
+        if 'active' in self._fields:
+            if self.active == True:
+                self.write({'active': False})
+                return True
+        return super().unlink()
