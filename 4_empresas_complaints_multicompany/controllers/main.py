3e963da952e50483cad8e43103207809feaa2a38
Dave <fernando.gam@outlook.com>
2026-03-10 17:45:50 -0500

    sended email from complaint

--- 4_empresas_complaints_multicompany/controllers/main.py
+++ 4_empresas_complaints_multicompany/controllers/main.py
@@ -351,9 +351,32 @@ class ComplaintMultiCompany(http.Controller):
 
         res_id = op_admission_model.sudo().create(real_values)
         res_id.reason_ids = [(6, 0, reason_arr)]
+        self._send_email_notify(res_id.id, res_id._name, [res_id.company_id.id])
         response = request.render('4_empresas_complaints_multicompany.claim_done', {})
         return response
 
+    def _send_email_notify(self, record_id, model_name, company_ids):
+        _logger.info("init send email")
+        group = request.env.ref('soy_cybersecurity_cybersecurity.group_cybersecurity_write_printreport', raise_if_not_found=False).sudo()
+        base_url = request.env['ir.config_parameter'].sudo().get_param('web.base.url')
+        record_url = f"{base_url}/web#id={record_id}&model={model_name}&view_type=form"
+
+        if group:
+            users = group.users.sudo().search([
+                ('company_id', 'in', company_ids),
+                ('report_incident', '=', True),
+            ])
+        else:
+            users = []
+            
+        _logger.info(f"users email: {users}")
+        for user in users:
+            mail_values = {
+                    "subject": f"Tiene un reclamo por revisar",
+                    "body_html": f"<p>Hola {user.display_name},</p><p>Tiene un reclamo por revisar</p> <p>Puedes verlo aquí: <a href={record_url}>Ver registro</a></p>",
+                    "email_to": user.email,
+                }
+            request.env["mail.mail"].sudo().create(mail_values).send()
 
     @http.route('/reclamos', type='http', auth='public', website=True, sitemap=True)
     def company_complaint_list(self, **kw):
